75af35ad20a5a925dea5d632932b4afd8f7be4e9
Eagan Martin <mece.martinece@gmail.com>
2018-09-07 18:10:41 +0800

    Updated gitignore; Updated database replicator;

--- app/src/main/java/com/example/reyanthonypaano/personfinder/database/manager/DBReplicator.java
+++ app/src/main/java/com/example/reyanthonypaano/personfinder/database/manager/DBReplicator.java
@@ -1,12 +1,19 @@
 package com.example.reyanthonypaano.personfinder.database.manager;
 
+import com.couchbase.lite.BasicAuthenticator;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.Endpoint;
+import com.couchbase.lite.Replicator;
+import com.couchbase.lite.ReplicatorChange;
+import com.couchbase.lite.ReplicatorChangeListener;
 import com.couchbase.lite.ReplicatorConfiguration;
 import com.couchbase.lite.URLEndpoint;
+import com.couchbase.lite.internal.support.Log;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+
+import static android.content.ContentValues.TAG;
 
 public class DBReplicator {
     private Endpoint targetEndpoint;
@@ -19,6 +26,21 @@ public class DBReplicator {
         }
 
         ReplicatorConfiguration replConfig = new ReplicatorConfiguration(database, this.targetEndpoint);
+
         replConfig.setReplicatorType(ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL);
+        replConfig.setAuthenticator(new BasicAuthenticator("admin", "admin"));
+
+        Replicator replicator = new Replicator(replConfig);
+
+        replicator.addChangeListener(new ReplicatorChangeListener() {
+            @Override
+            public void changed(ReplicatorChange change) {
+                if(change.getStatus().getError() != null) {
+                    Log.i(TAG, "Error code :: " + change.getStatus().getError().getCode());
+                }
+            }
+        });
+
+        replicator.start();
     }
 }
